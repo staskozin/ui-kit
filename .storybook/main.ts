e8455f5e2f9bc2e7e35d4d2dc25e7b0105d5ad5d
Stanislav Kozin <stas@staskozin.ru>
2026-03-14 23:06:36 +0300

    chore(Storybook): убран chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
     addons: [
-        '@chromatic-com/storybook',
         '@storybook/addon-vitest',
         '@storybook/addon-a11y',
         '@storybook/addon-docs',
